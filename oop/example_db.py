4d0bd3c113a9d8599d15a6e719d5656ed3625381
zaid kamil <xaidmetamorphos@gmail.com>
2022-04-01 18:24:26 +0530

    db exampe

--- oop/example_db.py
+++ oop/example_db.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Float
 from sqlalchemy.engine import create_engine
@@ -5,17 +6,21 @@ from datetime import datetime
 
 Base = declarative_base()
 
-class Pet(Base):
-    
-    __tablename__ = 'pets'
+class Task(Base):
+    __tablename__ = 'tasks'
     id = Column(Integer, primary_key=True)
-    name = Column(String(25))
-    age = Column(Integer)
-    pettype = Column(String(15))
-    owner = Column(String(25))
-    added_at = Column(DateTime, default=datetime.now())
+    name = Column(String)
+    created_at = Column(DateTime,default=datetime.now)
+
+class PythonNews(Base):
+    __tablename__ = 'python_news'
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    summary = Column(String)
+    link = Column(String)
+    added_on = Column(DateTime, default=datetime.now)
 
 if __name__ == "__main__":
-    engine =  create_engine('sqlite:///pets.sqlite3', echo=True)
+    engine =  create_engine('sqlite:///demo.sqlite3', echo=True)
     Base.metadata.create_all(engine)
     
