61fba849031c9b1536e01501549d190d00e32a04
zaid kamil <xaidmetamorphos@gmail.com>
2022-04-12 18:15:24 +0530

    flask form

--- flask_examples/app1.py
+++ flask_examples/app1.py
@@ -1,8 +1,20 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, request, flash, redirect
 
-@app.route('/')
+app = Flask(__name__)
+app.secret_key = 'some_secret'
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        website = request.form.get('website')
+        title =  request.form.get('title')
+        category = request.form.get('category')
+        has_ip = request.form.get('ip')
+        if not website or not title or not category:
+            flash('Please enter both a website and a title and category', 'danger')
+        else:
+            flash("the details have been saved", 'success')
+        return redirect('/')
     return render_template('index.html')
 
 @app.route('/about')
