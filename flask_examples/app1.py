7c5b5eee32b5787cc5d9c1c356382a064a786e01
zaid kamil <xaidmetamorphos@gmail.com>
2022-04-14 18:22:59 +0530

    database

--- flask_examples/app1.py
+++ flask_examples/app1.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template, request, flash, redirect
+from sqlalchemy.orm import sessionmaker
+from database import WebTest
+from sqlalchemy.engine import create_engine
+
 
 app = Flask(__name__)
 app.secret_key = 'some_secret'
+
+def opendb():
+    engine =  create_engine('sqlite:///app.sqlite3', echo=True)
+    Session = sessionmaker(bind=engine)
+    return Session()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -13,6 +22,12 @@ def index():
         if not website or not title or not category:
             flash('Please enter both a website and a title and category', 'danger')
         else:
+            has_ip = True if has_ip else False
+            db = opendb()
+            wt = WebTest(url=website,title=title,category=category,get_ip=has_ip)
+            db.add(wt)
+            db.commit()
+            db.close()
             flash("the details have been saved", 'success')
         return redirect('/')
     return render_template('index.html')
